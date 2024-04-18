4663c6743166c125bb02c5b8c8f0532189834eb4
spaceless <bodnarillia26@gmail.com>
2024-04-19 00:35:29 +0300

    initial commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,77 @@
+module.exports = {
+  root: true,
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  settings: {
+    react: {
+      version: "detect",
+    },
+    "import/resolver": {
+      node: {
+        paths: ["src"],
+        extensions: [".js", ".jsx", ".ts", ".tsx"],
+      },
+    },
+  },
+  env: {
+    browser: true,
+    amd: true,
+    node: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "eslint-config-prettier",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:react/recommended",
+    "plugin:jsx-a11y/recommended",
+    "eslint-config-prettier",
+    "plugin:prettier/recommended", // Make sure this is always the last element in the array.
+  ],
+  plugins: ["simple-import-sort", "prettier"],
+  rules: {
+    "prettier/prettier": [
+      "error",
+      { endOfLine: "auto" },
+      { usePrettierrc: true },
+    ],
+    "react/react-in-jsx-scope": "off",
+    "jsx-a11y/accessible-emoji": "off",
+    "react/prop-types": "off",
+    "@typescript-eslint/explicit-function-return-type": "off",
+    "simple-import-sort/imports": "error",
+    "simple-import-sort/exports": "error",
+    "jsx-a11y/anchor-is-valid": [
+      "error",
+      {
+        components: ["Link"],
+        specialLink: ["hrefLeft", "hrefRight"],
+        aspects: ["invalidHref", "preferButton"],
+      },
+    ],
+
+    semi: "off",
+    "react/jsx-filename-extension": ["error", { extensions: [".tsx"] }],
+    "react/jsx-props-no-spreading": "off",
+    "react/function-component-definition": [
+      2,
+      {
+        namedComponents: "arrow-function",
+        unnamedComponents: "arrow-function",
+      },
+    ],
+    "no-console": 0,
+    "linebreak-style": 0,
+    "import/no-named-as-default": 0,
+    "import/no-named-as-default-member": 0,
+    "jsx-a11y/label-has-associated-control": 0,
+    "import/prefer-default-export": "off",
+    "object-curly-newline": "off",
+  },
+};
